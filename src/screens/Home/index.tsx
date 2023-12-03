8d9ba4ce4ccf065ddbd530476702b40e0bba0b1b
William Roger <williamroger.frontend@gmail.com>
2023-12-03 17:50:08 -0300

    first commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Text } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+
+export function Home() {
+  return (
+    <>
+      <StatusBar style="auto" />
+      <Text>Todo App</Text>
+    </>
+  )
+}
